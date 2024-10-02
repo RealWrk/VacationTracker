d123d386e0acdc18c23ebb4b3ad670b715c571ac
Kevin Bronakowski <54969495+RealWrk@users.noreply.github.com>
2024-10-02 09:24:51 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,90 @@
+// Constants for the form and form controls
+const newVacationForm = document.getElementById("vacation-form");
+const startDateInput = document.getElementById("start-date");
+const endDateInput = document.getElementById("end-date");
+const pastVacationContainer = document.getElementById("past-vacations");
+
+// Listen to form submissions
+newVacationForm.addEventListener("submit", (event)=> {
+    // prevent form from submitting to the server
+    // since everything will be on client side
+    event.preventDefault();
+
+    // get dates from the form
+    const startDate = startDateInput.value;
+    const endDate = endDateInput.value;
+
+    // validate dates
+    if (checkDatesInvalid(startDate, endDate)) {
+        return; // don't submit the form, just exit
+    }
+
+    // store the new vacation in our client-side storage
+    storeNewVacation(startDate, endDate);
+
+    // refresh UI
+    renderPastVacations();
+
+    // reset the form
+    newVacationForm.reset();
+});
+
+function checkDatesInvalid(startDate, endDate) {
+    if (!startDate || !endDate || startDate > endDate) {
+        // error message
+        // we're just gonna clear the form if anything is invalid
+        newVacationForm.reset();
+
+        return true; // something is invalid
+    } else {
+        return false; // everything is good
+    }
+};
+
+// add the storage key as an app-wide constant
+const STORAGE_KEY = "vacation_tracker";
+
+function storeNewVacation(startDate, endDate) {
+    // get data from the storage
+    const vacations = getAllStoredVacations(); // returns an array of Strings
+    
+    // add new vacation (JSON object) at the end of the array
+    vacations.push({startDate, endDate});
+
+    // sort the array so newest to oldest
+    vacations.sort((a, b) => {
+        return new Date(b.startDate) - new Date(a.startDate)
+    });
+
+    // store the new array back in storage
+
+    window.localStorage.setItem(STORAGE_KEY, JSON.stringify(vacations));
+};
+
+function getAllStoredVacations(){
+    //get the string of vacation from localStorage
+    const data = window.localStorage.getItem(STORAGE_KEY);
+
+    //if no vacations are stored, default to an empty array
+    //other wise, return the stored data (JSON string) as parsed JSON
+    const vacations = data ? JSON.parse(data) : [];
+
+    return vacations;
+}
+
+function renderPastVacations() {
+    //get the parsed string of vacations or an empty array if there arent any 
+    const vacations = getAllStoredVacations();
+
+    //exit if there arent any vacations
+    if(vacations.length === 0){
+        return;
+    }
+
+    //clear the list of past vacations since we're going to re-render it
+    pastVacationContainer.innerHTML = "";
+
+    const pastVacationHeader = document.getElementById
+};
+
+
