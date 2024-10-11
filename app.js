463ba733c4e3a45529677f8b283b3722b3908072
RealWrk <kb4640@g.rit.edu>
2024-10-11 09:16:49 -0400

    current changes

--- app.js
+++ app.js
@@ -168,3 +168,27 @@ document.getElementById("sendButton").addEventListener("click", ()=>{
 let db;
 const dbName = "SyncDatabase";
 const request = indexedDB.open(dbName,1);
+
+request.onerror = function (event){
+    console.error("Database error: " + event.target.error);
+};
+
+request.onsuccess = function(event){
+    //now we actually have our db
+    db = event.target.result;
+    console.log("Database opened successfully");
+};
+
+request.onupgradeneeded = function (event){
+    db = event.target.result;
+
+    //create any new object stores for our db or delete any old ones from a previous version
+    const objectStore = db.createObjectStore("pendingData",
+        {
+            keyPath:"id",
+            autoIncrement: true
+        }
+    );
+};
+
+//add data to out db, we need a transaction to accomplish it
